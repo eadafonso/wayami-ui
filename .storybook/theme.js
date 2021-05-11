aefdd822db8d38db7e4e0b6be2eb9693c7ce41e4
Edvaldo Afonso <eadafonso@gmail.com>
2021-05-11 07:16:41 +0100

    create base and config project

---
+++ .storybook/theme.js
@@ -0,0 +1,20 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "dark",
+
+  colorSecondary: "#862a23",
+
+  appBg: "#0b0a0d",
+  appContentBg: "#16161A",
+  appBorderColor: "#862a23",
+
+  textColor: "#fff",
+
+  // Toolbar default and active colors
+  barTextColor: "#E6E6E6",
+  barSelectedColor: "#ffffff",
+  barBg: "#1b1a1f",
+
+  brandTitle: "Wayami-ui",
+});
